ffc530900741264d1dd68eb25b9be5f485a1eaf2
aybüke <77280855+cescedes@users.noreply.github.com>
2022-10-27 17:40:22 +0300

    Update two sum ii.py

--- two sum ii.py
+++ two sum ii.py
@@ -4,6 +4,9 @@
 #Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
 #The tests are generated such that there is exactly one solution. You may not use the same element twice.
 #Your solution must use only constant extra space.
+#Input: numbers = [2,7,11,15], target = 9
+#Output: [1,2]
+#Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
 
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
