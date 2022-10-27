c9e3a81bf0badd22410a501cebf162fa4f80e3b8
aybüke <77280855+cescedes@users.noreply.github.com>
2022-10-27 17:35:56 +0300

    Update first bad version.c

--- first bad version.c
+++ first bad version.c
@@ -4,7 +4,15 @@ Since each version is developed based on the previous version, all the versions 
 Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
 You are given an API bool isBadVersion(version) which returns whether version is bad.
 Implement a function to find the first bad version.
-You should minimize the number of calls to the API. */
+You should minimize the number of calls to the API. 
+Input: n = 5, bad = 4
+Output: 4
+Explanation:
+call isBadVersion(3) -> false
+call isBadVersion(5) -> true
+call isBadVersion(4) -> true
+Then 4 is the first bad version.
+*/
 
 // The API isBadVersion is defined for you.
 // bool isBadVersion(int version);
