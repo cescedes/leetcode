ee083cde4502e4c7d54645e944e7e30847038d22
aybüke <77280855+cescedes@users.noreply.github.com>
2022-10-27 17:38:28 +0300

    Update search insert position.c

--- search insert position.c
+++ search insert position.c
@@ -1,6 +1,9 @@
 /* Given a sorted array of distinct integers and a target value, return the index if the target is found.
 If not, return the index where it would be if it were inserted in order.
-You must write an algorithm with O(log n) runtime complexity. */
+You must write an algorithm with O(log n) runtime complexity. 
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+*/
 
 int searchInsert(int nums[], int numsSize, int target){
     for (int i=0;i<numsSize;i++)
