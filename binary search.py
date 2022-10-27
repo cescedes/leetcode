54b84c1ad4afbe72449dadfb37d13d57d476f888
aybüke <77280855+cescedes@users.noreply.github.com>
2022-10-27 17:35:12 +0300

    Update binary search.py

--- binary search.py
+++ binary search.py
@@ -2,6 +2,9 @@
 #write a function to search target in nums. If target exists, then return its index.
 #Otherwise, return -1.
 #You must write an algorithm with O(log n) runtime complexity.
+#Input: nums = [-1,0,3,5,9,12], target = 9
+#Output: 4
+#Explanation: 9 exists in nums and its index is 4
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
